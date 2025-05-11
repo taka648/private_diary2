e806201aa90025d180a5b307a6ddf0a5ea5b3122
taka648 <taka648@gmail.com>
2025-05-11 22:24:36 +0900

    first commit

--- private_diary_project/private_diary_project/settings.py
+++ private_diary_project/private_diary_project/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+# リスト7.1:静的ファイルの場所(パス)を設定する
+STATICFILES_DIRS = ( 
+    os.path.join(BASE_DIR, "static"),
+) 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
